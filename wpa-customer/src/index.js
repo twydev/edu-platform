69931e120b67da498c64d128358bef0e32e27feb
twydev <39231273+twydev@users.noreply.github.com>
2019-04-20 00:18:45 +0800

    added header and drawer. set up firebase project

--- wpa-customer/src/index.js
+++ wpa-customer/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './App';
+import SearchAppBar from './components/SearchAppBar';
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(<SearchAppBar />, document.getElementById('header'));
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
